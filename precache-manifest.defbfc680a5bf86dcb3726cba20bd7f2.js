1a29176f08cd795274ce56af8cbca6a2bb80cc6a
James Ku <jameskutw@gmail.com>
2019-12-05 02:47:32 -0500

    Updates

---
+++ precache-manifest.defbfc680a5bf86dcb3726cba20bd7f2.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af0aab1474269f8c24c8651df20d7f9",
+    "url": "/react-tiling-puzzle-solver/index.html"
+  },
+  {
+    "revision": "60771911e665f107cdaa",
+    "url": "/react-tiling-puzzle-solver/static/css/2.6fa1aeb7.chunk.css"
+  },
+  {
+    "revision": "a7e864abd13aa2503e7a",
+    "url": "/react-tiling-puzzle-solver/static/css/main.cdb4254e.chunk.css"
+  },
+  {
+    "revision": "60771911e665f107cdaa",
+    "url": "/react-tiling-puzzle-solver/static/js/2.0d23f3ae.chunk.js"
+  },
+  {
+    "revision": "a7e864abd13aa2503e7a",
+    "url": "/react-tiling-puzzle-solver/static/js/main.572c53fd.chunk.js"
+  },
+  {
+    "revision": "0316865cd8cd25c1441a",
+    "url": "/react-tiling-puzzle-solver/static/js/runtime-main.cc4c04f7.js"
+  },
+  {
+    "revision": "f4d7e2cc622e1922bae7636006fcc6c4",
+    "url": "/react-tiling-puzzle-solver/static/media/IQ_creator.f4d7e2cc.bin"
+  },
+  {
+    "revision": "80b506991f4d5510966648a1fc9d0d37",
+    "url": "/react-tiling-puzzle-solver/static/media/checkerboard.80b50699.bin"
+  },
+  {
+    "revision": "65780082061551a727d0ac296eccd6c8",
+    "url": "/react-tiling-puzzle-solver/static/media/lucky13.65780082.bin"
+  },
+  {
+    "revision": "b8406fefec53cd1903a3e765bc254567",
+    "url": "/react-tiling-puzzle-solver/static/media/new.b8406fef.bin"
+  },
+  {
+    "revision": "3d95faecb4a100254704274da3b10782",
+    "url": "/react-tiling-puzzle-solver/static/media/pentominoes3x20.3d95faec.bin"
+  },
+  {
+    "revision": "5a7fe64877d9df0468a88e8b43f4f59f",
+    "url": "/react-tiling-puzzle-solver/static/media/pentominoes4x15.5a7fe648.bin"
+  },
+  {
+    "revision": "365b0a886c5f73be3d030a2858db0e9e",
+    "url": "/react-tiling-puzzle-solver/static/media/pentominoes5x12.365b0a88.bin"
+  },
+  {
+    "revision": "368f41dc16d386f780aa24b051fe1bdb",
+    "url": "/react-tiling-puzzle-solver/static/media/pentominoes6x10.368f41dc.bin"
+  },
+  {
+    "revision": "ed2af10ebfc2e0c5c9df6a28906b662d",
+    "url": "/react-tiling-puzzle-solver/static/media/pentominoes8x8_middle_missing.ed2af10e.bin"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/react-tiling-puzzle-solver/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "6f90874e00facf63d72af7f09e1d7ecd",
+    "url": "/react-tiling-puzzle-solver/static/media/test1.6f90874e.bin"
+  },
+  {
+    "revision": "113c1996751499fba2c3077e4dc40d35",
+    "url": "/react-tiling-puzzle-solver/static/media/test2.113c1996.bin"
+  },
+  {
+    "revision": "104c44bfcb4e4ec28dc97aeb1c1f3981",
+    "url": "/react-tiling-puzzle-solver/static/media/thirteen_holes.104c44bf.bin"
+  },
+  {
+    "revision": "08a061dfdc78f8bf40864890e25987da",
+    "url": "/react-tiling-puzzle-solver/static/media/trivial.08a061df.bin"
+  },
+  {
+    "revision": "df61a3b04a523b6a9f8937efd78d38e7",
+    "url": "/react-tiling-puzzle-solver/static/media/trivial2.df61a3b0.bin"
+  }
+]);
